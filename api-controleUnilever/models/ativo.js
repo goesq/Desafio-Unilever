b612d601e2ee21d7db7246fec9b8bb74d1f77070
Pedro Guerra <pguerra872@gmail.com>
2024-10-30 22:02:55 -0300

    Add back-end

---
+++ api-controleUnilever/models/ativo.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose'); // Variável mongoose que irá armazenar a conexão futura com o MongoDB
+
+// Define a estrutura para salvar o produto no banco de dados
+const AtivoSchema = new mongoose.Schema({
+    // nome, tipo string, required indica que o parâmetro é obrigatório
+    nome: {
+        type: String,
+        required: true
+    },
+    quantidade: {
+        type: Number,
+        required: true
+    },
+    valor: {
+        type: Number,
+        required: true
+    },
+    lote:{
+        type: Number,
+        required: true
+    }
+});
+
+// Exportando o modelo para salvar os ativos
+module.exports = mongoose.model('Ativo', AtivoSchema);
+
